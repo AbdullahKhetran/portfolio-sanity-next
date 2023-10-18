37ac1da59ff9d099192942856accbf58ec39de2e
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-18 06:58:56 +0500

    integrated Sanity

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+
+export default defineConfig({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  title: "Portfolio Site Hackathon",
+  basepath: "/admin",
+  plugins: [deskTool(), visionTool()],
+  schema: { types: schemaTypes },
+})
