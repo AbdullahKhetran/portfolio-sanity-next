69f23772bb51acf41409c7a376362de7e041c696
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-23 02:26:13 +0500

    minor whitespace differences

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -2,19 +2,19 @@ import { groq } from "next-sanity";
 import client from "./sanity.client";
 
 export async function getProfile() {
-    return client.fetch(
-        groq`*[_type == "profile"]{
-      _id,
-      fullName,
-      headline,
-      profileImage {alt, "image": asset->url},
-      shortBio,
-      location,
-      fullBio,
-      email,
-      "resumeURL": resumeURL.asset->url,
-      socialLinks,
-      skills
-    }`
-    );
+  return client.fetch(
+    groq`*[_type == "profile"]{
+          _id,
+          fullName,
+          headline,
+          profileImage {alt, "image": asset->url},
+          shortBio,
+          location,
+          fullBio,
+          email,
+          "resumeURL": resumeURL.asset->url,
+          socialLinks,
+          skills
+        }`
+  );
 }
