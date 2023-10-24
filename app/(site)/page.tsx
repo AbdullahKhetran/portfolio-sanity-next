13f953a4c72f01076c79e2a7ad05e8461a22ad00
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-25 00:22:47 +0500

    added Job component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,15 @@
 import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
 import HeroSvg from "@/icons/HeroSvg";
+import Job from "@/components/Job";
 
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
 
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
+
+      {/* Profile Section */}
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
         {profile &&
           profile.map((data) => (
@@ -35,6 +38,11 @@ export default async function Home() {
             </div>
           ))}
         <HeroSvg />
+      </section>
+
+      {/* Job History Section */}
+      <section>
+        <Job />
       </section>
     </main>
   );
